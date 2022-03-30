34c180d9927a300958e8ab1b236ccab503dcf2b4
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-03-30 14:45:49 -0400

    Update code.py

--- code.py
+++ code.py
@@ -6,16 +6,17 @@
  
 import ugame
 import stage
- 
+
 def game_scene ():
 # this function is the main game game_scene
-     
+
     print("\n\n\n") # 3 blank lines
     print("Hello, World!")
      
     # repeat forever, game loop
     while True:
         pass # just a placeholder for now
+
     
 if __name__ == "__main__":
     game_scene()
