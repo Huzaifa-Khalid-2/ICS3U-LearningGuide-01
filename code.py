564851bcd1d0d5b8e876c7588a6bc0f0740ff3b5
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-01 14:25:52 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,8 +4,8 @@
 # Created on: March 2022
 # This program is the "Space Aliens" program onn the PyBadge
 
-import ugame
 import stage
+import ugame
 
 def game_scene ():
 # this function is the main game game_scene
@@ -16,6 +16,7 @@ def game_scene ():
     # repeat forever, game loop
     while True:
     pass # just a placeholder for now
+
 
 if __name__ == "__main__":
     game_scene()
