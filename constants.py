3316adb756615a048e5f38ddd81665bc86765fa1
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-11 14:02:06 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Huzaifa Khalid
+# Created on: April 2022
+# This constants file is for the space aliens game
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE SIZE = 16
+FPS = 60
+SPRITE_MOVE_FORWARD_SPEED = 1
