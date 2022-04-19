60a8a227b139facc1b0aee5ad7bceea4d0e7fef3
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-19 13:57:21 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
-SPRITE_MOVEMENT_SPEED = 1
+SPRITE_MOVE_FORWARD_SPEED = 1
 
 # Using for button state
 button_state = {
@@ -21,3 +21,5 @@ button_state = {
     "button_still_pressed": "still pressed",
     "button_released": "released",
 }
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
