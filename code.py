5177e32546eda51b02bb62d0d8ff90605aaed1c5
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-22 14:17:07 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,10 +5,74 @@
 # This program is the "Space Aliens" program onn the PyBadge
 
 import constants
+import random
 import stage
+import time
 import ugame
 
 
+def splash_scene():
+    # this function is the splash scene
+    
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+    
+    # an image bank for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y)
+    
+    # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+    
+    # create a stage for the background to show up on 
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = [background]
+    # render the background and intial location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
+    
+    # repeat forever, game loop
+    while True:
+        # Wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
 def menu_scene():
     # this function is the main game game_scene
 
@@ -56,7 +120,6 @@ def menu_scene():
 
         # update game logic
         game.tick()  # wait until refresh rate finishes
-
 
 def game_scene():
     # this function is the main game game_scene
@@ -123,9 +186,9 @@ def game_scene():
         if keys & ugame.K_X != 0:
             pass
         if keys & ugame.K_START != 0:
-            print("Start")
+            pass
         if keys & ugame.K_START != 0:
-            print("Select")
+            pass
 
         if keys & ugame.K_RIGHT != 0:
             if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
@@ -155,4 +218,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
