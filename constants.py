ff4a730a44c5a64733659761bcda2aaabeaf18da
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-05-16 16:24:46 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -12,27 +12,30 @@ SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 TOTAL_NUMBER_OF_LASERS = 5
+NEOPIXEL_COUNT = 5
+NEOPIXEL_BRIGHTNESS = 10
 SHIP_SPEED = 1
 ALIEN_SPEED = 1
 LASER_SPEED = 2
 OFF_SCREEN_X = -100
 OFF_SCREEN_Y = -100
 OFF_TOP_SCREEN = -1 * SPRITE_SIZE
-OFF_BUTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
 # Using for button state
 button_state = {
     "button_up": "up",
-    "button_just_pressed": "just pressed",
-    "button_still_pressed": "still pressed",
-    "button_released": "released"
+    "button_just_pressed": "just_pressed",
+    "button_still_pressed": "still_pressed",
+    "button_released": "released",
 }
-RED_PALETTE = (
-    b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
-    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
-)
+
+# new pallete for red filled text
+RED_PALETTE = (b'\xf8\x1f\x00\x00\xcey\x00\xff\xf8\x1f\xff\x19\xfc\xe0\xfd\xe0'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff')
 
 # new pallete for blue filled text
 WHI_PALETTE = (b'\xf8\x1f\x00\x00\xcey\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
